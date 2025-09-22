a89de00e0b998bc2f1f0cf9cba14750ae2367bac
Md-Redwan <redwanhosen4@gamil.com>
2025-09-23 01:23:46 +0600

    Now New Arrival Responsive For All Device

--- src/Component/NewArrival.jsx
+++ src/Component/NewArrival.jsx
@@ -11,28 +11,28 @@ const NewArrival = () => {
     <div className="mt-28 mb-20">
       <Container>
         <TitleCard titleName="Featured" titleHeading="New Arrival" />
-        <div className="mt-15 lg:flex lg:justify-between justify-center">
+        <div className="mt-15 flex flex-col items-center lg:flex-row lg:justify-evenly xl:justify-between">
           <div>
-            <img src={PS5} alt="" className="cursor-pointer" />
+            <img src={PS5} alt="" className="cursor-pointer h-auto lg:w-[450px] xl:w-[570px]" />
           </div>
           <div className="mt-5 lg:mt-0">
             <div>
-              <img src={WomenCollections} alt="" className="cursor-pointer" />
+              <img src={WomenCollections} alt="" className="cursor-pointer lg:w-[500px] xl:w-[570px]" />
             </div>
-            <div className="flex justify-center">
-              <div className="flex gap-5 lg:justify-between mt-8">
+            
+            <div className="flex justify-evenly lg:justify-between mt-5 xl:mt-8">
                 <img
                   src={Speakers}
                   alt=""
-                  className="cursor-pointer w-35 sm:w-43  md:w-67.5 h-auto"
+                  className="cursor-pointer w-35 sm:w-43 md:w-60 lg:w-50 xl:w-67.5 h-auto lg:h-[200px] xl:h-auto"
                 />
                 <img
                   src={Perfume}
                   alt=""
-                  className="cursor-pointer w-35 sm:w-43  md:w-67.5 h-auto"
+                  className="cursor-pointer w-35 sm:w-43 md:w-60 lg:w-55 h-[] xl:w-67.5 h-auto lg:h-[200px] xl:h-auto"
                 />
-              </div>
             </div>
+            
           </div>
         </div>
       </Container>
