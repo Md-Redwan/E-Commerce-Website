547cf4eeb9b829d3f762bea7f206023cbf22476c
Md-Redwan <redwanhosen4@gamil.com>
2025-10-03 12:14:43 +0600

    Product Details Page updated

--- src/ProductDetailsPage/G92Gamepad.jsx
+++ src/ProductDetailsPage/G92Gamepad.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "../Component/Header";
 import NavBar from "../Component/NavBar";
 import Container from "../Component/Container";
@@ -8,8 +7,29 @@ import G92pic2 from "../assets/G92pic2.png";
 import G92pic3 from "../assets/G92pic3.png";
 import G92pic4 from "../assets/G92pic4.png";
 import { FaStar } from "react-icons/fa";
+import { FiMinus } from "react-icons/fi";
+import { FiPlus } from "react-icons/fi";
+import React, { useState } from "react";
+import Flex from "../Component/Flex";
+import { IoHeart } from "react-icons/io5";
+
 
 const G92Gamepad = () => {
+  const [heart, setHeart] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const handleHeartClick = () => {
+    setHeart(!heart);
+  };
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="font-[poppins]">
       <Header />
@@ -31,7 +51,7 @@ const G92Gamepad = () => {
               <img src={G92picMain} alt="" />
             </div>
           </div>
-          <div className="w-[30%]">
+          <div className="w-[35%]">
             <h2 className="font-semibold font-[inter] text-2xl leading-6 tracking-[3px]">
               Havic HV G-92 Gamepad
             </h2>
@@ -86,6 +106,30 @@ const G92Gamepad = () => {
                 </div>
               </div>
             </div>
+            <Flex className="justify-between items-center  mt-6">
+              <div className="flex justify-between items-center w-[159px] border-1 border-gray-500 rounded-sm">
+                <div className="flex justify-center items-center h-11 w-10 text-2xl border-r-1 border-gray-500 hover:bg-[#DB4444] hover:text-white">
+                  <FiMinus onClick={decrement} />
+                </div>
+                <p className="font-[poppins leading-7 text-[20px] font-medium">
+                  {count}
+                </p>
+                <div className="flex justify-center items-center h-11 w-10 text-2xl border-l-1 border-gray-500 hover:bg-[#DB4444] hover:text-white">
+                  <FiPlus onClick={increment} />
+                </div>
+              </div>
+              <button className="py-2.5 px-12 border-1 border-gray-500 rounded-sm hover:bg-[#DB4444] hover:text-white">
+                Buy Now
+              </button>
+              <div className="w-11 h-11 border-1 border-gray-500 flex justify-center items-center rounded-sm">
+                <IoHeart
+                  onClick={handleHeartClick}
+                  className={`w-8 h-8 cursor-pointer text-black  transition-colors ${
+                    heart ? "text-red-500" : ""
+                  }`}
+                />
+              </div>
+            </Flex>
           </div>
         </div>
       </Container>
